67443521ce01d30118d92460da5467a5aec597c1
Kumari Shivani Singh <kumarishivanisingh14@gmail.com>
2020-07-28 00:29:28 +0530

    Connected MongDB/mongoose

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const PORT = 3000;
+
+mongoose.connect("mongodb://127.0.0.1:27017", { useNewUrlParser: true }, (error)=> {
+    if(!error){
+        console.log("Success Connected!");
+    } else{
+        console.log("Error connecting to database!")
+    }
+});
+
+app.use(express.static('public'))
 
 app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
