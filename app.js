38f7c52b21b7781c4f4d911b53ee2aa7a2042c25
Kumari Shivani Singh <kumarishivanisingh14@gmail.com>
2020-08-01 01:11:48 +0530

    Setup essential middlewares cors, body-parser & morgan

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const app = express();
+
 const PORT = 3000;
 
-
-mongoose.connect("mongodb://127.0.0.1:27017", { useNewUrlParser: true }, (error)=> {
-    if(!error){
+mongoose.connect("mongodb://127.0.0.1:27017", { useNewUrlParser: true }, (error) => {
+    if (!error) {
         console.log("Success Connected!");
-    } else{
+    } else {
         console.log("Error connecting to database!")
     }
 });
 
+app.use(cors())
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(morgan('dev'))
+
 app.use(express.static('public'))
 
 app.listen(PORT, () => {
-  console.log(`Listening on PORT ${PORT}`);
+    console.log(`Listening on PORT ${PORT}`);
 });
