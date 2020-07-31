912fccd0be7dc94a5e3a652f10448d88e6cc8092
ritika-0111 <51761546+ritika-0111@users.noreply.github.com>
2020-08-01 01:34:09 +0530

    #8

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const path = require('path');
+const multer  = require('multer');
 const mongoose = require("mongoose");
 const PORT = 3000;
 
 
+// mongodb connection
 mongoose.connect("mongodb://127.0.0.1:27017", { useNewUrlParser: true }, (error)=> {
     if(!error){
         console.log("Success Connected!");
@@ -19,6 +21,18 @@ app.use(express.static('public'))
 // set the view engine to ejs
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// set storage 
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '/public')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now())
+    }
+  })
+   
+var upload = multer({ storage: storage })
 
 // index page 
 app.get('/', function(req, res) {
