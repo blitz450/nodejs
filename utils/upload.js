314224e7e102f9a7e1a28cab5f4c9d8779bc609a
ritika-0111 <51761546+ritika-0111@users.noreply.github.com>
2020-08-06 02:23:41 +0530

    add

--- utils/upload.js
+++ utils/upload.js
@@ -1,14 +1,17 @@
+var path = require("path");
 const multer  = require('multer');
 
 // set storage 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, '/public')
+      cb(null, './public')
     },
     filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now())
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     }
-  })
+  })  
+
+
 
 
   module.exports = multer({ storage });
