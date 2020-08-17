c954ac2c7545fc98110268f6017cb6f6b66bfca2
ritika-0111 <51761546+ritika-0111@users.noreply.github.com>
2020-08-18 01:27:13 +0530

    #43

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const session = require('express-session');
 let Data = require('./models/resource');
 let User = require('./models/users');
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
+dotenv.config();
+
 
 const PORT = 3000;
 
@@ -84,6 +88,57 @@ app.get('/about', function (req, res) {
 app.get('/contact', function (req, res) {
     res.render('contact', {user: req.session});
 });
+
+//contact form data
+app.post('/contact', function (req, res) {
+  const output = `
+    <p>You have a new Query.</p>
+    <h3>Contact Details</h3>
+    <ul>  
+      <li>Name: ${req.body.name}</li>
+      <li>Email: ${req.body.email}</li>
+      <li>Phone: ${req.body.phone}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: process.env.mailing_email, // generated ethereal user
+        pass: process.env.mailing_email_password  // generated ethereal password
+    },
+    tls:{
+      rejectUnauthorized:false
+    }
+  });
+
+    // setup email data with unicode symbols
+  let mailOptions = {
+    from: `"Team-NJ2" <${process.env.mailing_email}>`, // sender address
+    to: process.env.recipient_email, // list of receivers
+    subject: 'Contact Details', // Subject line
+    text: 'Contact Details', // plain text body
+    html: output // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+       } else {
+
+          console.log('Message sent: %s', info.messageId);   
+          console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+          res.redirect('/');
+
+       }
+    });
+})
 
 // login page 
 app.get('/login', function (req, res) {
